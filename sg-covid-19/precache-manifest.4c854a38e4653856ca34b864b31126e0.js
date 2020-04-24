e09abb7f88ba9fabe550c19e3612ffebbf4a5521
Huasheng <huashengwang1989@gmail.com>
2020-04-24 23:13:14 +0800

    Site updated: 2020-04-24 23:13:14

---
+++ sg-covid-19/precache-manifest.4c854a38e4653856ca34b864b31126e0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bac9183e4c31d40d88170acdb218d9d2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f6b303c38fcafe0c189b",
+    "url": "/static/css/2.d788185a.chunk.css"
+  },
+  {
+    "revision": "0317078adc3c8219eb06",
+    "url": "/static/css/main.78d5b7ff.chunk.css"
+  },
+  {
+    "revision": "f6b303c38fcafe0c189b",
+    "url": "/static/js/2.76887599.chunk.js"
+  },
+  {
+    "revision": "51723a250c78a0342129647892a3f700",
+    "url": "/static/js/2.76887599.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0317078adc3c8219eb06",
+    "url": "/static/js/main.37b9ac5b.chunk.js"
+  },
+  {
+    "revision": "5e3d1140ddeda2a85a1e",
+    "url": "/static/js/runtime-main.550e1575.js"
+  }
+]);
